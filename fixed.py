91fdecd17b2c3609864ed08822d07ddc0d192aab
nalrzoog <n.alrzoog@gmail.com>
2026-07-18 20:11:31 +0300

    wrap totals logic in total_price function

--- fixed.py
+++ fixed.py
@@ -1,5 +1,9 @@
-prices = [10, 25, 7, 42]
-total = 0
-for i in range(len(prices)):
-    total = total + prices[i]
-print("Total:", total)
+
+
+def total_price(prices):
+    total = 0
+    for price in prices:
+        total = total + price
+    return total
+
+print("Total:", total_price([10, 25, 7, 42]))
